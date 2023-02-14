65fb6ae0798a298136aaad6e2e3e928aec544ad2
markahofmeister <markhof12@gmail.com>
2023-02-14 12:17:01 -0500

    Assignment 7 complete. Beginning lab 2.

--- _assignment-7-BubbleSortBlink/BubbleSort_Blink.c
+++ _assignment-7-BubbleSortBlink/BubbleSort_Blink.c
@@ -13,17 +13,35 @@
 #include "..\inc\LaunchPad.h"
 #include "..\inc\Clock.h"
 
-volatile uint32_t Time, MainCount;      // semaphores (I think)
+//volatile uint32_t Time, MainCount;      // semaphores (I think)
+volatile uint8_t switchInput;             // semaphore to determine what action the interrupt handler should take
+
+uint8_t Port1_Input(void){
+  return (P1->IN&0x12);                 // read P1.4,P1.1 inputs
+}
+
+void SysTick_Handler(void){                 //handles interrupt to blink one of the LEDs
+
+    if(switchInput == 0x10) {               //If SW1 has been depressed, blink red LED (will be 8 Hz)
+        P1->OUT ^= 0x01;
+    }
+    else if(switchInput == 0x02) {          //If SW2 has been depressed, blink blue LED (will be 64 Hz)
+        P2->OUT ^= 0x04;
+    }
+    else {                                  //If no input, turn off all LEDs
+        P1->OUT &= ~0x01;
+        P2->OUT &= ~0x04;
+    }
+}
 
 
 void main(void)
 {
-
-    Time = MainCount = 0;
     Clock_Init48MHz();              // running on crystal
     LaunchPad_Init();               // P1.0 is red LED on LaunchPad, P2.0-2.2 are RGB LED. Switches are on 1.1 and P1.4.
+    SysTick_Init(6000000,2);
     EnableInterrupts();             //Enable interrupts from SysTick
-    SysTick_Init(6000000,2);        // set up SysTick for 8 Hz interrupts
+    P1->OUT &= ~0x01;               //Turn off LED initially
 
     uint32_t i, j, temp, length = 100;      //variables to do bubble sorting
 
@@ -41,6 +59,26 @@ void main(void)
     while(1) {
 
         for (i = 0; i < length; i++) {
+
+            //Read switch values using conditional
+            //reload SysTick value based on conditional
+            //proceed to inner for loop
+
+            //uint8_t switchInput = Port1_Input();
+            switchInput = Port1_Input();
+
+            if(switchInput == 0x02) {       //if switch 1 has been pressed
+
+                SysTick_Init(93750,2);        // set up SysTick for 64 Hz interrupts
+
+            }
+            else if(switchInput == 0x10) {  //if switch 2 has been pressed
+
+                SysTick_Init(6000000,2);          //set up SysTick for 8 Hz interrupts
+
+            }
+            // (Switches seem to be reversed on my board. IDK why.)
+
             for (j = 0; j < length - i - 1; j++) {
                 if (a[j + 1] < a[j]) {
 
@@ -52,21 +90,12 @@ void main(void)
             }           //end inner for loop
 
             WaitForInterrupt();         //Pause sorting to wait for light blinking interrupt
-            //P1->OUT ^= 0x01;
-            MainCount++;
+            //MainCount++;
 
         }               //end outer for loop
 
     }
 
-}
 
-void SysTick_Handler(void){                 //handles an interrupt to blink red LED
-  P2->OUT ^= 0x04;       // toggle P2.2
 }
-
-uint8_t Port1_Input(void){
-  return (P1->IN&0x12);   // read P1.4,P1.1 inputs
-}
-
 
